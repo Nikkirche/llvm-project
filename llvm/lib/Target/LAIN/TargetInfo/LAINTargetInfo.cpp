28e7c4608b0e6358a0dbf05d4630b004526b5e80
Nikolay <nikkirche@hotmail.com>
2024-04-11 00:21:46 +0300

    description fix

--- llvm/lib/Target/LAIN/TargetInfo/LAINTargetInfo.cpp
+++ llvm/lib/Target/LAIN/TargetInfo/LAINTargetInfo.cpp
@@ -1,13 +1,16 @@
 #include "TargetInfo/LAINTargetInfo.h"
+#include "LAIN.h"
 #include "llvm/MC/TargetRegistry.h"
 using namespace llvm;
 
 Target &llvm::getTheLAINTarget() {
+  LAIN_DUMP_YELLOW
   static Target TheLAINTarget;
   return TheLAINTarget;
 }
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeLAINTargetInfo() {
-  RegisterTarget<Triple::lain> X(getTheLAINTarget(), "LAIN", "LAIN 32",
-                                   "lain");
+  LAIN_DUMP_YELLOW
+  RegisterTarget<Triple::lain> X(getTheLAINTarget(), "lain","Simple learning architecture",
+                                   "LAIN");
 }
